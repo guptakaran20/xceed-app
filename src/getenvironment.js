e56cd3837217e396ee316cda129578bad6e370e9
guptakaran0720 <guptakaran0720@gmail.com>
2026-08-08 19:31:30 +0530

    production url

--- src/getenvironment.js
+++ src/getenvironment.js
@@ -6,7 +6,7 @@ function getEnvironment() {
   const androidEmulator = 'http://10.0.2.2:8010';
 
   if (window.Capacitor && window.Capacitor.isNativePlatform()) {
-    return androidEmulator;
+    return nitjServer;
   } else if (currentURL.includes('localhost') || currentURL.includes('127.0.0.1')) {
     return development;
   } else if (currentURL.includes('nitjtt')) {
